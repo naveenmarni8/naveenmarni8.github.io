2b4c075257c4445fa4331105f168671ce709cec4
naveenmarni8 <48897553+naveenmarni8@users.noreply.github.com>
2020-02-23 14:53:35 +0530

    update

---
+++ h.js
@@ -0,0 +1,27 @@
+function dark()
+{
+   document.getElementsByTagName("h1")[0].setAttribute("id","dark");
+   document.body.style.backgroundColor="black";
+   document.getElementById("head").setAttribute("id","headd")
+   document.getElementById("mid").style.backgroundColor="black";
+   var x=document.getElementsByTagName("button");
+   document.getElementsByClassName("cente")[0].style.color="#F0E68C";
+   var i;
+   for(i=0;i<x.length;i++)
+   {
+   	x[i].style.backgroundColor="black";
+   	x[i].style.color="white";
+   }
+   x=document.getElementsByTagName("a");
+   for(i=0;i<x.length;i++)
+   {
+   	x[i].style.color="green";
+   }
+   document.getElementById("lb1").style.color="white";
+   x=document.getElementsByTagName("h4");
+   for(i=0;i<x.length;i++)
+   {
+   	x[i].style.color="white";
+   }
+document.getElementsByClassName("at")[0].style.color="white";
+ }
